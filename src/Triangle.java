d77ce79992bcde4cb66ef117cb91fe1441e9c574
Michał Szymecki <szymsonik@gmail.com>
2020-01-27 21:48:07 +0100

    Cosmetic patches

--- src/Triangle.java
+++ src/Triangle.java
@@ -11,12 +11,14 @@ public class Triangle {
         return ((triangle.a + triangle.b > triangle.c) && (triangle.a + triangle.c > triangle.b) && (triangle.b
                 + triangle.c > triangle.a));
     }
-    boolean isRightTriangle(Triangle triangle){
-        return ((square(triangle.a)+square(triangle.b)==square(triangle.c)) ||
-                (square(triangle.c)+square(triangle.b)==square(triangle.a)) ||
-                (square(triangle.a)+square(triangle.c)==square(triangle.b))); //it can be done better
+
+    boolean isRightTriangle(Triangle triangle) {
+        return ((square(triangle.a) + square(triangle.b) == square(triangle.c)) ||
+                (square(triangle.c) + square(triangle.b) == square(triangle.a)) ||
+                (square(triangle.a) + square(triangle.c) == square(triangle.b))); //it can be done better
     }
-    double square(double a){
-        return a*a;
+
+    double square(double a) {
+        return a * a;
     }
 }
--- src/TriangleTest.java
+++ src/TriangleTest.java
@@ -1,18 +1,16 @@
 public class TriangleTest {
     public static void main(String[] args) {
 
-        Triangle triangle = new Triangle(-3,-100,5);
+        Triangle triangle = new Triangle(-3, -100, 5);
 
 
-        if(triangle.isThisTriangle(triangle)){
-            if(triangle.isRightTriangle(triangle)){
+        if (triangle.isThisTriangle(triangle)) {
+            if (triangle.isRightTriangle(triangle)) {
                 System.out.println("It is a right triangle");
-            }
-            else{
+            } else {
                 System.out.println("It isn't a right triangle");
             }
-        }
-        else{
+        } else {
             System.out.println("This is not a triangle");
         }
     }
